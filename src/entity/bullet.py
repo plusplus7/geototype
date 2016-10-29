83aaec130a57ba9315d42cf62e4ef1ed07aeb389
+7 <jackiedeng.cn@gmail.com>
2016-10-29 10:53:05 +0800

    添加实体类和少许测试

---
+++ src/entity/bullet.py
@@ -0,0 +1,32 @@
+# -*- coding: UTF-8 =*=
+
+import math
+
+class Bullet():
+
+    def __init__(self, x, y, degree, length, speed):
+        self.x  = x
+        self.y  = y
+        self.dg = degree
+        self.l  = length
+        self.sp = speed
+
+        self.dx = self.sp * math.sin(math.radians(self.dg))
+        self.dy = self.sp * math.cos(math.radians(self.dg))
+
+        self.__points = self.__generate_points()
+
+    def move(self):
+        for pt in self.__points:
+            pt[0] = pt[0] + self.dx
+            pt[1] = pt[1] + self.dy
+
+    def points(self):
+        return self.__points
+
+    def __generate_points(self):
+        return [
+            [self.x, self.y],
+            [self.x + self.l * math.sin(math.radians(self.dg)),
+             self.y + self.l * math.cos(math.radians(self.dg))],
+        ]
