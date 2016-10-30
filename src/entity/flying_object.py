03249e51c5c09a47e8895f050adbd349db01181d
+7 <jackiedeng.cn@gmail.com>
2016-10-30 15:28:07 +0800

    Fix flying object

--- src/entity/flying_object.py
+++ src/entity/flying_object.py
@@ -1,31 +1,32 @@
 # -*- coding: UTF-8 -*-
 
+QUADRANGLE = "quadrangle"
+
 class FlyingObject():
-    QUADRANGLE = "quadrangle"
     def __init__(self, x, y, speed, obj_type, health):
         self.x = x
         self.y = y
         self.speed  = speed
         self.health = health
-        self.points = self.__generate_points(obj_type)
+        self.__points = self.__generate_points(obj_type)
 
     def move(self, degree):
         dx = self.speed * math.sin(math.radians(degree))
         dy = self.speed * math.cos(math.radians(degree))
-        for pt in points:
+        for pt in self.__points:
             pt[0] = pt[0] + dx
             pt[1] = pt[1] + dy
 
     def points(self):
-        return self.points
+        return self.__points
 
     def __generate_points(self, obj_type):
-        if cmp(QUADRANGLE, obj_type):
+        if cmp(QUADRANGLE, obj_type) == 0:
             return [
-                [x-2, y-2],
-                [x+2, y-2],
-                [x+2, y+2],
-                [x-2, y+2],
+                [self.x-2, self.y-2],
+                [self.x+2, self.y-2],
+                [self.x+2, self.y+2],
+                [self.x-2, self.y+2],
             ]
         else:
             raise Exception('Unsupported object type')
