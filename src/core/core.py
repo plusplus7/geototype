d683b86ee962f292b75d12dfbf6067a1d60dab33
+7 <jackiedeng.cn@gmail.com>
2016-11-13 16:17:09 +0800

    去掉所有的注释

--- src/core/core.py
+++ src/core/core.py
@@ -1,11 +1,6 @@
 # -*- coding: UTF-8 -*-
 
 import math
-#from .. import entity
-#from ..entity import bullet
-#from ..entity import flying_object
-#from ..entity.bullet import Bullet
-#from ..entity.flying_object import FlyingObject
 
 class Core():
     def __init__(self):
@@ -21,27 +16,12 @@ class Core():
 
 
     def check_collision(self, fobj, bobj):
-        '''self.flying_object.x = fobj.x
-        self.flying_object.y = fobj.y
-        self.flying_object.speed = fobj.speed
-        self.flying_object.obj_type = fobj.obj_type
-        '''
         self.flying_object.health = fobj.health
-        '''self.bullet.x = bobj.x
-        self.bullet.y =bobj.y
-        self.bullet.dg = bobj.dg
-        self.bullet.l = bobj.l
-        self.bullet.sp = bobj.sp
-        '''
         self.bullet.dmg = bobj.dmg
-        '''
-        self.bullet.dx = bobj.dx
-        self.bullet.dy = bobj.dy'''
         flag = 0
 
         def distance(x1, x2, y1, y2):
             return (x2 - x1) ** 2 + (y2 - y1) ** 2
-        # print distance(bobj.x,fobj.x,bobj.y,fobj.y)
         if distance(bobj.x,fobj.x,bobj.y,fobj.y) <= 2*math.sqrt(2):
                 flag = 1
                 self.flying_object.health -= 1
